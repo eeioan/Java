d09716112b90e289e62f1b71994b539eb93411f0
eeioness <doctorsmokingaaz@gmail.com>
2025-09-23 02:03:28 +0300

    Summarizing its Amazing!

--- java-solutions/Sum.java
+++ java-solutions/Sum.java
@@ -1,4 +1,37 @@
-public class Sum {
+public class Sum { 
     public static void main(String[] args) {
+        int totalSum = 0;
+        
+        for (String arg : args ) {
+            StringBuilder sBuilder = new StringBuilder(); //string builder собирает символы в строчку через
+            boolean negative = false; 
+            for (int j = 0; j < arg.length(); j++) {
+                char symbol = arg.charAt(j);
+                if (Character.isWhitespace(symbol)) {
+                    if (sBuilder.length() > 0 ) {
+                        int number = Integer.parseInt(sBuilder.toString()); // 
+                        if (negative) number = -number;
+                        totalSum += number;
+                        sBuilder.setLength(0);
+                        negative = false; 
+                    }
+                } else if (symbol == '-' && sBuilder.length() == 0) {
+                    negative = true;
+                } else if (  '0' <= symbol && symbol <= '9') { // java не умеет в двойное сравнение (((
+                    sBuilder.append(symbol);
+                } else {
+                    sBuilder.setLength(0); //отсекаем лищние символы
+                    negative = false ;
+                }
+            }
+            if (sBuilder.length() > 0 ) {
+                int number = Integer.parseInt(sBuilder.toString());
+                if (negative) number = -number;
+                    totalSum += number;
+                    sBuilder.setLength(0);
+                    negative = false; 
+                }
+        }
+        System.out.println(totalSum);
     }
 }
