71736f11d0ac57186d8a7b70ca4b0b9886ee9bb3
Nikolay Vedernikov <VedernikovNV@gmail.com>
2025-10-07 17:37:49 +0300

    Checked by an instructor

--- java-solutions/SumLongOctal.java
+++ java-solutions/SumLongOctal.java
@@ -11,28 +11,22 @@ public class SumLongOctal {
                     continue; //if (i >= arg.length()) break // 
                 }
                 sBuilder.setLength(0);
-                if (arg.charAt(i) == '-' || arg.charAt(i) == '+' ) { //+ и - можно записывать к числу 
-                    sBuilder.append(arg.charAt(i));
-                    i++;
-                    if (i >= arg.length() || !Character.isDigit(arg.charAt(i))) {  
-                        continue;
-                    }
-                }
-                while (i < arg.length() && Character.isDigit(arg.charAt(i))) {
+
+                while (i < arg.length() && !Character.isWhitespace(arg.charAt(i))) {
                     sBuilder.append(arg.charAt(i));
                     i++;
                 }
                 int radix = 10;
-                if (i < arg.length() && (arg.charAt(i) == 'o' || arg.charAt(i) == 'O')) {
-                    radix = 8;  
-                    i++;             
+                if (i < arg.length() && (arg.charAt(i - 1) == 'o' || arg.charAt(i - 1) == 'O')) {
+                    radix = 8;
+                    sBuilder.deleteCharAt(sBuilder.length() - 1);
+                    i++;
                 }
-                if (sBuilder.length() > 0 && sBuilder.charAt(0) == '-' ) {
+                if (!sBuilder.isEmpty() && sBuilder.charAt(0) == '-') {
                     totalSum += Long.parseLong(sBuilder.toString(), radix);
-                }else{
+                } else {
                     totalSum += Long.parseUnsignedLong(sBuilder.toString(), radix); //если переполнен не - Long
                 }
-
             }
         }
         System.out.println(totalSum);
